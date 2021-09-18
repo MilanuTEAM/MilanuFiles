c8365f42bf5b2f7121b6ff6cc28396235cce9771
MilanuTEAM <90847166+MilanuTEAM@users.noreply.github.com>
2021-09-18 14:06:32 +0300

    Update ReplyBot.lua

--- MilanuFiles/ReplyBot.lua
+++ MilanuFiles/ReplyBot.lua
@@ -1,7 +1,7 @@
 local function Reply(msg)
 local text = msg.content_.text_
 if ChatType == 'sp' or ChatType == 'gp'  then
-if not DeAlsh:get(DeProx..'Alsh:Lock:Reply'..msg.chat_id_) then
+if not DeAlsh:get(Milanu..'Alsh:Lock:Reply'..msg.chat_id_) then
 if text == "انجب" or text == "نجب" or text == "انجبي" or text == "نجبي" or text == "انجبو" or text == "نجبو" then
 if SudoId(msg.sender_user_id_) then
 rd = 'مطوريي اغلط شكد متريد نورتنه ، ♥️💪🏿'
@@ -31,92 +31,92 @@ end
 De_Alsh(msg.chat_id_, msg.id_, 1,rd, 1, 'html')
 end
 if text == 'دي' or text == 'ديي' or text == 'دي بابه' then 
-DeProxTEAM =  "آخلُِآقٌڪڪ لُِڪڪ 𖠙 😒🔪"
-De_Alsh(msg.chat_id_, msg.id_, 1, DeProxTEAM, 1, 'md') 
+MilanuTEAM =  "آخلُِآقٌڪڪ لُِڪڪ 𖠙 😒🔪"
+De_Alsh(msg.chat_id_, msg.id_, 1, MilanuTEAM, 1, 'md') 
 return false
 end
 if text == 'السلام عليكم' or text == 'سلام عليكم' then 
-DeProxTEAM =  "ياھَہّلْاا ۅﻋ̝̚لْيڪْم الْﺳ̭͠ لْام 𖠙 🤤♥️"
-De_Alsh(msg.chat_id_, msg.id_, 1, DeProxTEAM, 1, 'md') 
+MilanuTEAM =  "ياھَہّلْاا ۅﻋ̝̚لْيڪْم الْﺳ̭͠ لْام 𖠙 🤤♥️"
+De_Alsh(msg.chat_id_, msg.id_, 1, MilanuTEAM, 1, 'md') 
 return false
 end
 if text == '😭💔' or text == '😭😭' or text == '😭😭😭' or text == '😿💔' or text == '😭😭😭' or text == '😭😭😭😭' then 
-DeProxTEAM =  "مآآعٍآشُ آلُِي يزعٍلُِڪڪ 𖠙 😏♥️"
-De_Alsh(msg.chat_id_, msg.id_, 1, DeProxTEAM, 1, 'md') 
+MilanuTEAM =  "مآآعٍآشُ آلُِي يزعٍلُِڪڪ 𖠙 😏♥️"
+De_Alsh(msg.chat_id_, msg.id_, 1, MilanuTEAM, 1, 'md') 
 return false
 end
 if text == '🌚💔' or text == '💔🌚' or text == '🚶‍♂💔' or text == '💔' or text == '😔💔' or text == '🚶‍♀💔' or text == '😭' then 
-DeProxTEAM =  "شبّيي اﻟ̣̣פﻟ̣̣و 𖠙 😿💔"
-De_Alsh(msg.chat_id_, msg.id_, 1, DeProxTEAM, 1, 'md') 
+MilanuTEAM =  "شبّيي اﻟ̣̣פﻟ̣̣و 𖠙 😿💔"
+De_Alsh(msg.chat_id_, msg.id_, 1, MilanuTEAM, 1, 'md') 
 return false
 end
 if text == 'باي' or text == 'بااي' or text == 'اروح' or text == 'اروح احسن' or text == 'اولي احسن' or text == 'راح اروح' or text == 'باي انام' then 
-DeProxTEAM =  "أُرٌجُعُ عٍيدِهآآ مٍوو تنِْسةه 𖠙 🤤♥️"
-De_Alsh(msg.chat_id_, msg.id_, 1, DeProxTEAM, 1, 'md') 
+MilanuTEAM =  "أُرٌجُعُ عٍيدِهآآ مٍوو تنِْسةه 𖠙 🤤♥️"
+De_Alsh(msg.chat_id_, msg.id_, 1, MilanuTEAM, 1, 'md') 
 return false
 end
 if text == 'هلو' or text == 'هلاو' or text == 'هلا' or text == 'هلاوو' or text == 'هيلاو' or text == 'هيلاوو' or text == 'هلاا' then 
-local DeProxTEAM = {"اࠗط็لق֯ق֯ هٞللاࠗ୨و 𖠙 🤤♥️" ,"هۂَلاا نۨہٰورتت يعمۘري 𖠙 🤤♥️" } 
-DeAlsh2 = math.random(#DeProxTEAM) 
-De_Alsh(msg.chat_id_, msg.id_, 1, DeProxTEAM[DeAlsh2] , 1, 'md') 
+local MilanuTEAM = {"اࠗط็لق֯ق֯ هٞللاࠗ୨و 𖠙 🤤♥️" ,"هۂَلاا نۨہٰورتت يعمۘري 𖠙 🤤♥️" } 
+DeAlsh2 = math.random(#MilanuTEAM) 
+De_Alsh(msg.chat_id_, msg.id_, 1, MilanuTEAM[DeAlsh2] , 1, 'md') 
 return false
 end
 if text == 'شونك' or text == 'شونج' or text == 'شلونك' or text == 'شلونج' or text == 'شونكم' or text == 'شلونكم' or text == 'شلخبار' or text == 'شلون الاخبار' or text == 'شخبارك' then 
-local DeProxTEAM = {"ماﺷ͠ يةھَہّ يﻋ̝̚مريي ۅاﻧﺗ̲ت 𖠙 🤤♥️" ,"الحۡمۘدللهۂَ ٰوانۨہتت 𖠙 🤤♥️","تمۘامۘ عمۘري ٰوانۨہتت 𖠙 🤤♥️"} 
-DeAlsh2 = math.random(#DeProxTEAM) 
-De_Alsh(msg.chat_id_, msg.id_, 1, DeProxTEAM[DeAlsh2] , 1, 'md') 
+local MilanuTEAM = {"ماﺷ͠ يةھَہّ يﻋ̝̚مريي ۅاﻧﺗ̲ت 𖠙 🤤♥️" ,"الحۡمۘدللهۂَ ٰوانۨہتت 𖠙 🤤♥️","تمۘامۘ عمۘري ٰوانۨہتت 𖠙 🤤♥️"} 
+DeAlsh2 = math.random(#MilanuTEAM) 
+De_Alsh(msg.chat_id_, msg.id_, 1, MilanuTEAM[DeAlsh2] , 1, 'md') 
 return false
 end
 if text == 'وينك' or text == 'وينج' then
-DeProxTEAM =  "مْوٌجہوٌدِ يہمْگُ يحلوُ 𖠙 😉♥️"
-De_Alsh(msg.chat_id_, msg.id_, 1, DeProxTEAM, 1, 'md') 
+MilanuTEAM =  "مْوٌجہوٌدِ يہمْگُ يحلوُ 𖠙 😉♥️"
+De_Alsh(msg.chat_id_, msg.id_, 1, MilanuTEAM, 1, 'md') 
 return false
 end
 if text == 'بوت عواي' or text == 'بوت زربه' or text == 'البوت عاوي' or text == 'البوت زربه' then
-DeProxTEAM =  "اطردكك تجرب ؟ ، 😕🔪"
-De_Alsh(msg.chat_id_, msg.id_, 1, DeProxTEAM, 1, 'md') 
+MilanuTEAM =  "اطردكك تجرب ؟ ، 😕🔪"
+De_Alsh(msg.chat_id_, msg.id_, 1, MilanuTEAM, 1, 'md') 
 return false
 end
 if text == 'نايمين' or text == 'ميتين' then
-DeProxTEAM =  "طُآمسين ووعٍيوونڪ 𖠙 😪🖤ۦ"
-De_Alsh(msg.chat_id_, msg.id_, 1, DeProxTEAM, 1, 'md') 
+MilanuTEAM =  "طُآمسين ووعٍيوونڪ 𖠙 😪🖤ۦ"
+De_Alsh(msg.chat_id_, msg.id_, 1, MilanuTEAM, 1, 'md') 
 return false
 end
 if text == 'هلوباي' or text == 'هلو باي' then  
-DeProxTEAM =  "شحۡسۜيت مۘنۨہ هيجۚ ּكتبت ؟ 🌝♥️"
-De_Alsh(msg.chat_id_, msg.id_, 1, DeProxTEAM, 1, 'md') 
+MilanuTEAM =  "شحۡسۜيت مۘنۨہ هيجۚ ּكتبت ؟ 🌝♥️"
+De_Alsh(msg.chat_id_, msg.id_, 1, MilanuTEAM, 1, 'md') 
 return false
 end
 if text == 'اكلك' or text == 'اكلج' or text == 'اكلكم' then 
-DeProxTEAM =  "ڪوولُِ مآڪوولُِ لُِآحٍدِ 𖠙 😉♥️"
-De_Alsh(msg.chat_id_, msg.id_, 1, DeProxTEAM, 1, 'md')
+MilanuTEAM =  "ڪوولُِ مآڪوولُِ لُِآحٍدِ 𖠙 😉♥️"
+De_Alsh(msg.chat_id_, msg.id_, 1, MilanuTEAM, 1, 'md')
 return false
 end
 if text == 'فرخ' then
-DeProxTEAM =  "ٰوينۨہهۂَ خۡل احۡصرهۂَ 𖠙 😹♥️" 
-De_Alsh(msg.chat_id_, msg.id_, 1, DeProxTEAM, 1, 'md') 
+MilanuTEAM =  "ٰوينۨہهۂَ خۡل احۡصرهۂَ 𖠙 😹♥️" 
+De_Alsh(msg.chat_id_, msg.id_, 1, MilanuTEAM, 1, 'md') 
 return false
 end
 if text == 'سورس عبس' or text == 'سورس بروكس' or text == 'سورس ديف بروكس' or text == 'سورس زربه' or text == 'السورس زربه' or text == 'سورس عاوي' or text == 'السورس عاوي' then 
-DeProxTEAM =  "لُِآ سوورس خآلُِتڪ دِي لُِڪ 𖠙 😒🔪" 
-De_Alsh(msg.chat_id_, msg.id_, 1, DeProxTEAM, 1, 'md')
+MilanuTEAM =  "لُِآ سوورس خآلُِتڪ دِي لُِڪ 𖠙 😒🔪" 
+De_Alsh(msg.chat_id_, msg.id_, 1, MilanuTEAM, 1, 'md')
 return false
 end
 end
 
 if Manager(msg) then
 if text == 'تفعيل ردود البوت' or text == 'تفعيل الردود' then
-if not DeAlsh:get(DeProx..'Alsh:Lock:Reply'..msg.chat_id_) then
+if not DeAlsh:get(Milanu..'Alsh:Lock:Reply'..msg.chat_id_) then
 De_Alsh(msg.chat_id_, msg.id_, 1, '⌁︙ردود البوت بالتاكيد مفعله ', 1, 'md')
 else
 De_Alsh(msg.chat_id_, msg.id_, 1, '⌁︙تم تفعيل ردود البوت', 1, 'md')
-DeAlsh:del(DeProx..'Alsh:Lock:Reply'..msg.chat_id_)
+DeAlsh:del(Milanu..'Alsh:Lock:Reply'..msg.chat_id_)
 end end
 if text == 'تعطيل ردود البوت' or text == 'تعطيل الردود' then
-if DeAlsh:get(DeProx..'Alsh:Lock:Reply'..msg.chat_id_) then
+if DeAlsh:get(Milanu..'Alsh:Lock:Reply'..msg.chat_id_) then
 De_Alsh(msg.chat_id_, msg.id_, 1, '⌁︙ردود البوت بالتاكيد معطله ', 1, 'md')
 else
-DeAlsh:set(DeProx..'Alsh:Lock:Reply'..msg.chat_id_,true)
+DeAlsh:set(Milanu..'Alsh:Lock:Reply'..msg.chat_id_,true)
 De_Alsh(msg.chat_id_, msg.id_, 1, '⌁︙تم تعطيل ردود البوت', 1, 'md')
 end end
 end
