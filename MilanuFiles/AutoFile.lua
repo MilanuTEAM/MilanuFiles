6b7db13610af922993aa962136d01340e3e9fd86
MilanuTEAM <90847166+MilanuTEAM@users.noreply.github.com>
2021-09-18 14:27:15 +0300

    Add files via upload

---
+++ MilanuFiles/AutoFile.lua
@@ -0,0 +1,124 @@
+local function AutoFile(msg)
+local text = msg.content_.text_
+if Sudo(msg) then
+if text == 'تفعيل النسخه التلقائيه' or text == 'تفعيل جلب نسخه الكروبات' or text == 'تفعيل عمل نسخه للمجموعات' then   
+De_Alsh(msg.chat_id_,msg.id_, 1, "⌁︙تم تفعيل جلب نسخة الكروبات التلقائيه\n⌁︙سيتم ارسال نسخه تلقائيه للكروبات كل يوم الى خاص المطور الاساسي", 1, 'md')
+DeAlsh:del(Milanu.."Alsh:Lock:AutoFile")
+end
+if text == 'تعطيل النسخه التلقائيه' or text == 'تعطيل جلب نسخه الكروبات' or text == 'تعطيل عمل نسخه للمجموعات' then  
+De_Alsh(msg.chat_id_,msg.id_, 1, "⌁︙تم تعطيل جلب نسخة الكروبات التلقائيه", 1, 'md')
+DeAlsh:set(Milanu.."Alsh:Lock:AutoFile",true) 
+end 
+end
+
+if (text and not DeAlsh:get(Milanu.."Alsh:Lock:AutoFile")) then
+Time = DeAlsh:get(Milanu.."Alsh:AutoFile:Time")
+if Time then 
+if Time ~= os.date("%x") then 
+local list = DeAlsh:smembers(Milanu..'Alsh:Groups') 
+local BotName = (DeAlsh:get(Milanu.."Alsh:NameBot") or 'بروكس')
+local GetJson = '{"BotId": '..Milanu..',"BotName": "'..BotName..'","GroupsList":{'  
+for k,v in pairs(list) do 
+LinkGroups = DeAlsh:get(Milanu.."Alsh:Groups:Links"..v)
+Welcomes = DeAlsh:get(Milanu..'Alsh:Groups:Welcomes'..v) or ''
+AlshConstructors = DeAlsh:smembers(Milanu..'Alsh:AlshConstructor:'..v)
+BasicConstructors = DeAlsh:smembers(Milanu..'Alsh:BasicConstructor:'..v)
+Constructors = DeAlsh:smembers(Milanu..'Alsh:Constructor:'..v)
+Managers = DeAlsh:smembers(Milanu..'Alsh:Managers:'..v)
+Admis = DeAlsh:smembers(Milanu..'Alsh:Admins:'..v)
+Vips = DeAlsh:smembers(Milanu..'Alsh:VipMem:'..v)
+if k == 1 then
+GetJson = GetJson..'"'..v..'":{'
+else
+GetJson = GetJson..',"'..v..'":{'
+end
+if #Vips ~= 0 then 
+GetJson = GetJson..'"Vips":['
+for k,v in pairs(Vips) do
+if k == 1 then
+GetJson =  GetJson..'"'..v..'"'
+else
+GetJson =  GetJson..',"'..v..'"'
+end
+end   
+GetJson = GetJson..'],'
+end
+if #Admis ~= 0 then
+GetJson = GetJson..'"Admis":['
+for k,v in pairs(Admis) do
+if k == 1 then
+GetJson =  GetJson..'"'..v..'"'
+else
+GetJson =  GetJson..',"'..v..'"'
+end
+end   
+GetJson = GetJson..'],'
+end
+if #Managers ~= 0 then
+GetJson = GetJson..'"Managers":['
+for k,v in pairs(Managers) do
+if k == 1 then
+GetJson =  GetJson..'"'..v..'"'
+else
+GetJson =  GetJson..',"'..v..'"'
+end
+end   
+GetJson = GetJson..'],'
+end
+if #Constructors ~= 0 then
+GetJson = GetJson..'"Constructors":['
+for k,v in pairs(Constructors) do
+if k == 1 then
+GetJson =  GetJson..'"'..v..'"'
+else
+GetJson =  GetJson..',"'..v..'"'
+end
+end   
+GetJson = GetJson..'],'
+end
+if #BasicConstructors ~= 0 then
+GetJson = GetJson..'"BasicConstructors":['
+for k,v in pairs(BasicConstructors) do
+if k == 1 then
+GetJson =  GetJson..'"'..v..'"'
+else
+GetJson =  GetJson..',"'..v..'"'
+end
+end   
+GetJson = GetJson..'],'
+end
+if #AlshConstructors ~= 0 then
+GetJson = GetJson..'"AlshConstructors":['
+for k,v in pairs(AlshConstructors) do
+if k == 1 then
+GetJson =  GetJson..'"'..v..'"'
+else
+GetJson =  GetJson..',"'..v..'"'
+end
+end   
+GetJson = GetJson..'],'
+end
+if LinkGroups then
+GetJson = GetJson..'"LinkGroups":"'..LinkGroups..'",'
+end
+GetJson = GetJson..'"Welcomes":"'..Welcomes..'"}'
+end
+GetJson = GetJson..'}}'
+local File = io.open('./'..Milanu..'.json', "w")
+File:write(GetJson)
+File:close()
+local abbas = 'https://api.telegram.org/bot' .. TokenBot .. '/sendDocument'
+local curl = 'curl "' .. abbas .. '" -F "chat_id='..DeId..'" -F "document=@'..Milanu..'.json' .. '" -F "caption=⌁︙يحتوي الملف على ↫ '..#list..' مجموعه"'
+io.popen(curl)
+io.popen('fm -fr '..Milanu..'.json')
+DeAlsh:set(Milanu.."Alsh:AutoFile:Time",os.date("%x"))
+end
+else 
+DeAlsh:set(Milanu.."Alsh:AutoFile:Time",os.date("%x"))
+end
+end
+
+end
+return {
+Milanu = AutoFile
+}
---
+++ MilanuFiles/ProNames.lua
@@ -0,0 +1,73 @@
+local function ProNames(msg)
+local text = msg.content_.text_
+if ChatType == 'sp' or ChatType == 'gp'  then
+if text and (text:match("^وضع توحيد (.*)$") or text:match("^ضع توحيد (.*)$")) then
+if Manager(msg) then
+if DeAlsh:get(Milanu.."Alsh:Lock:ProNames"..msg.chat_id_) then
+local Txt = text:match("^وضع توحيد (.*)$") or text:match("^ضع توحيد (.*)$")
+send(msg.chat_id_, msg.id_,'⌁︙تم تعيين ↫ '..Txt..' كتوحيد للمجموعه')
+DeAlsh:set(Milanu.."Alsh:ProNames:Txt"..msg.chat_id_,Txt)
+else
+send(msg.chat_id_, msg.id_,'⌁︙ميزة التوحيد معطله يرجى تفعيلها')
+end
+end
+end
+if text and (text:match("^تعين عدد الكتم (.*)$") or text:match("^تعيين عدد الكتم (.*)$")) then
+if Manager(msg) then
+if DeAlsh:get(Milanu.."Alsh:Lock:ProNames"..msg.chat_id_) then
+local Num = text:match("^تعين عدد الكتم (.*)$") or text:match("^تعيين عدد الكتم (.*)$")
+send(msg.chat_id_, msg.id_,'⌁︙تم تعيين  ↫ '..Num..' عدد الكتم')
+DeAlsh:set(Milanu.."Alsh:ProNames:Num"..msg.chat_id_,Num)
+else
+send(msg.chat_id_, msg.id_,'⌁︙ميزة التوحيد معطله يرجى تفعيلها')
+end
+end
+end
+if DeAlsh:get(Milanu.."Alsh:Lock:ProNames"..msg.chat_id_) then
+if text == "التوحيد" or text == "توحيد" then
+if DeAlsh:get(Milanu.."Alsh:ProNames:Txt"..msg.chat_id_) then
+local ProNamesTxt = DeAlsh:get(Milanu.."Alsh:ProNames:Txt"..msg.chat_id_)
+local ProNamesNum = DeAlsh:get(Milanu.."Alsh:ProNames:Num"..msg.chat_id_) or 5
+send(msg.chat_id_, msg.id_,'⌁︙التوحيد هو ↫ '..ProNamesTxt..'\n⌁︙عدد المحاولات للكتم ↫ '..ProNamesNum)
+else
+send(msg.chat_id_, msg.id_,'⌁︙لم يتم تعيين توحيد للمجموعه')
+end
+end
+end
+if not msg.forward_info_ and not Constructor(msg) then
+if DeAlsh:get(Milanu.."Alsh:Lock:ProNames"..msg.chat_id_) and DeAlsh:get(Milanu.."Alsh:ProNames:Txt"..msg.chat_id_) then 
+tdcli_function ({ID = "GetUser",user_id_ = msg.sender_user_id_},function(extra,result,success)
+if result and result.first_name_ then 
+if result.first_name_:match("(.*)"..DeAlsh:get(Milanu.."Alsh:ProNames:Txt"..msg.chat_id_).."(.*)") then 
+DeAlsh:srem(Milanu..'Alsh:Muted:'..msg.chat_id_, msg.sender_user_id_)
+else
+local ProNamesTxt = DeAlsh:get(Milanu.."Alsh:ProNames:Num"..msg.chat_id_) or 5
+local UserNum = DeAlsh:get(Milanu.."Alsh:ProNames:UserNum"..msg.chat_id_..msg.sender_user_id_) or 0
+if (tonumber(UserNum) == tonumber(ProNamesTxt) or tonumber(UserNum) > tonumber(ProNamesTxt)) then 
+DeAlsh:sadd(Milanu..'Alsh:Muted:'..msg.chat_id_, msg.sender_user_id_)
+DeAlsh:del(Milanu.."Alsh:ProNames:UserNum"..msg.chat_id_..msg.sender_user_id_)
+send(msg.chat_id_, msg.id_,"⌁︙العضو ↫ ["..result.first_name_.."](https://t.me/"..(result.username_ or "De_Prox")..")\n⌁︙تم كتمه بسبب عدم وضع توحيد المجموعه بجانب اسمه يجب عليه وضع التوحيد وسوف يتم الغاء كتمه تلقائيا")
+else 
+DeAlsh:incrby(Milanu.."Alsh:ProNames:UserNum"..msg.chat_id_..msg.sender_user_id_,1)
+send(msg.chat_id_, msg.id_, "⌁︙عذرا عزيزي ↫ ["..result.first_name_.."](https://t.me/"..(result.username_ or "De_Prox")..")\n⌁︙عليك وضع التوحيد ↫ `"..DeAlsh:get(Milanu.."Alsh:ProNames:Txt"..msg.chat_id_).."` بجانب اسمك\n⌁︙عدد المحاولات المتبقيه ↫ "..(tonumber(ProNamesTxt) - tonumber(UserNum)).."")
+end
+end
+end
+end,nil)
+end
+end
+
+if text == "تفعيل التوحيد" and Constructor(msg) then
+send(msg.chat_id_, msg.id_, '⌁︙تم تفعيل توحيد المجموعه')
+DeAlsh:set(Milanu.."Alsh:Lock:ProNames"..msg.chat_id_,true)
+end
+if text == "تعطيل التوحيد" and Constructor(msg) then
+send(msg.chat_id_, msg.id_, '⌁︙تم تعطيل توحيد المجموعه')
+DeAlsh:del(Milanu.."Alsh:Lock:ProNames"..msg.chat_id_)
+end
+end
+
+end
+return {
+Milanu = ProNames
+}
